868ed96eb1f79ab0f1637f53f6d0694cad67fc16
mukky <bellom962@gmail.com>
2020-09-02 09:21:13 +0100

    Mine

---
+++ index.php
@@ -0,0 +1,209 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>POI | Point of Interest</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+        <!-- All Plugin Css --> 
+		<link rel="stylesheet" href="css/plugins.css">
+		
+		<!-- Style & Common Css --> 
+		<link rel="stylesheet" href="css/common.css">
+        <link rel="stylesheet" href="css/main.css">
+
+    </head>
+	
+    <body>
+	
+		<!-- Navigation Start  -->
+		<nav class="navbar navbar-default navbar-sticky bootsnav">
+
+			<div class="container">      
+				<!-- Start Header Navigation -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+						<i class="fa fa-bars"></i>
+					</button>
+					<a class="navbar-brand" href="index.html">POI</a>
+				</div>
+				<!-- End Header Navigation -->
+
+				<!-- Nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="navbar-menu">
+					<ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
+							<li><a href="index.html">Home</a></li> 
+							<li><a href="login.php">Add Places</a></li>
+						</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>   
+		</nav>
+		<!-- Navigation End  -->
+		
+		<!-- Main jumbotron for a primary marketing message or call to action -->
+		<section class="main-banner" style="background:#242c36 url(img/background-picture.jpg) no-repeat">
+			<div class="container">
+				<div class="caption">
+					<h2>Visit Amazing places</h2>
+					<form>
+						<fieldset>
+							<div class="col-md-4 col-sm-4 no-pad">
+								<select class="selectpicker border-right">
+								  <option>Country</option>
+								  <option>0 Year</option>
+								  <option>1 Year</option>
+								  <option>2 Year</option>
+								  <option>3 Year</option>
+								</select>
+							</div>
+							<div class="col-md-3 col-sm-3 no-pad">
+								<select class="selectpicker border-right">
+								  <option>Region</option>
+								  <option>0 Year</option>
+								  <option>1 Year</option>
+								  <option>2 Year</option>
+								  <option>3 Year</option>
+								  <option>4 Year</option>
+								</select>
+							</div>
+							<div class="col-md-3 col-sm-3 no-pad">
+								<select class="selectpicker">
+								  <option>Type</option>
+								  <option>Town</option>
+								  <option>City</option>
+								</select>
+							</div>
+							<div class="col-md-2 col-sm-2 no-pad">
+								<input type="submit" class="btn seub-btn" value="Search" />
+							</div>
+						</fieldset>
+					</form>
+				</div>
+			</div>
+		</section>
+		
+		<section class="features">
+			<div class="container">
+				<div class="col-md-4 col-sm-4">
+					<div class="features-content">
+						<a href="#">
+							<img src="img/newyork.jpg" alt="Amazing places in Newyork" class="img-responsive">
+							<p class="font-weight-bold">Amazing Places in Newyork, USA</p>
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-4">
+					<div class="features-content">
+						<a href="#">
+							<img src="img/London.jpg" alt="Amazing places in Newyork" class="img-responsive">
+							<p class="font-weight-bold">Amazing Places in London, United Kingdom</p>
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-4">
+					<div class="features-content">
+						<a href="#">
+							<img src="img/Rome.jpg" alt="Amazing places in Newyork" class="img-responsive">
+							<p class="font-weight-bold">Amazing Places in Rome, Italy</p>
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-4">
+					<div class="features-content">
+						<a href="#">
+							<img src="img/Germany.jpg" alt="Amazing places in Newyork" class="img-responsive">
+							<p class="font-weight-bold">Amazing Places in FrankFurt, Germany</p>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4">
+					<div class="features-content">
+						<a href="#">
+							<img src="img/Paris.jpg" alt="Amazing places in Newyork" class="img-responsive">
+							<p class="font-weight-bold">Amazing Places in Paris, France</p>
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-4">
+					<div class="features-content">
+						<a href="#">
+							<img src="img/Belgium.jpg" alt="Amazing places in Newyork" class="img-responsive">
+							<p class="font-weight-bold">Amazing Places in Brussels, Belgium</p>
+						</a>
+					</div>
+				</div>
+			
+			</div>
+		</section>
+		<section class="testimonials dark">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div id="testimonial-slider" class="owl-carousel">
+							<div class="testimonial">
+								<div class="pic">
+									<img src="img/client-1.jpg" alt="">
+								</div>
+								<p class="description">
+									" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi eligendi facilis itaque minus non odio, quaerat ullam unde voluptatum? "
+								</p>
+								<h3 class="testimonial-title">williamson</h3>
+								<span class="post">Web Developer</span>
+							</div>
+
+							<div class="testimonial">
+								<div class="pic">
+									<img src="img/client-2.jpg" alt="">
+								</div>
+								<p class="description">
+									" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi eligendi facilis itaque minus non odio, quaerat ullam unde voluptatum? "
+								</p>
+								<h3 class="testimonial-title">kristiana</h3>
+								<span class="post">Web Designer</span>
+							</div>
+							
+							<div class="testimonial">
+								<div class="pic">
+									<img src="img/client-3.jpg" alt="">
+								</div>
+								<p class="description">
+									" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi eligendi facilis itaque minus non odio, quaerat ullam unde voluptatum? "
+								</p>
+								<h3 class="testimonial-title">kristiana</h3>
+								<span class="post">Web Designer</span>
+							</div>
+							
+							<div class="testimonial">
+								<div class="pic">
+									<img src="img/client-4.jpg" alt="">
+								</div>
+								<p class="description">
+									" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi eligendi facilis itaque minus non odio, quaerat ullam unde voluptatum? "
+								</p>
+								<h3 class="testimonial-title">kristiana</h3>
+								<span class="post">Web Designer</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		
+		
+			
+		
+		 
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+		<script src="js/bootsnav.js"></script>
+		<script src="js/main.js"></script>
+    </body>
+</html>
