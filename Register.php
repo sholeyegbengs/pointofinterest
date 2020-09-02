11df73d571bdbb046d89d261b36d51921529889d
mukky <bellom962@gmail.com>
2020-09-02 12:55:03 +0100

    Merge

--- Register.php
+++ Register.php
@@ -6,43 +6,43 @@
         <title>POI | Register</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-		
-        <!-- All Plugin Css --> 
+
+        <!-- All Plugin Css -->
 		<link rel="stylesheet" href="css/plugins.css">
-		
-		<!-- Style & Common Css --> 
+
+		<!-- Style & Common Css -->
 		<link rel="stylesheet" href="css/common.css">
         <link rel="stylesheet" href="css/main.css">
 
     </head>
-	
+
     <body>
-	
+
 		<!-- Navigation Start  -->
 		<nav class="navbar navbar-default navbar-sticky bootsnav">
 
-			<div class="container">      
+			<div class="container">
 				<!-- Start Header Navigation -->
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
 						<i class="fa fa-bars"></i>
 					</button>
-					<a class="navbar-brand" href="index.html"><div style="font-weight: bolder; font-size: 1.8em;">PointOfInterest</div></a>
+					<a class="navbar-brand" href="index.php"><div style="font-weight: bolder; font-size: 1.8em;">PointOfInterest</div></a>
 				</div>
 				<!-- End Header Navigation -->
 
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="navbar-menu">
 					<ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
-							<li><a href="index.php">Home</a></li> 
+							<li><a href="index.php">Home</a></li>
 							<li><a href="login.php">Login</a></li>
-							
+
 						</ul>
 				</div><!-- /.navbar-collapse -->
-			</div>   
+			</div>
 		</nav>
 		<!-- Navigation End  -->
-		
+
 		<!-- login section start -->
 		<section class="login-wrapper">
 			<div class="container">
@@ -59,7 +59,7 @@
 				</div>
 			</div>
 		</section>
-		<!-- login section End -->	
+		<!-- login section End -->
 		<script type="text/javascript" src="js/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="js/owl.carousel.min.js"></script>
--- index.php
+++ index.php
@@ -6,84 +6,66 @@
         <title>POI | Point of Interest</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-		
-        <!-- All Plugin Css --> 
+
+        <!-- All Plugin Css -->
 		<link rel="stylesheet" href="css/plugins.css">
-		
-		<!-- Style & Common Css --> 
+
+		<!-- Style & Common Css -->
 		<link rel="stylesheet" href="css/common.css">
         <link rel="stylesheet" href="css/main.css">
 
     </head>
-	
+
     <body>
-	
+
 		<!-- Navigation Start  -->
 		<nav class="navbar navbar-default navbar-sticky bootsnav">
 
-			<div class="container">      
+			<div class="container">
 				<!-- Start Header Navigation -->
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
 						<i class="fa fa-bars"></i>
 					</button>
-					<a class="navbar-brand" href="index.html">POI</a>
+					<a class="navbar-brand" href="index.php">POI</a>
 				</div>
 				<!-- End Header Navigation -->
 
 				<!-- Nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="navbar-menu">
 					<ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
-							<li><a href="index.html">Home</a></li> 
+							<li><a href="index.php">Home</a></li>
 							<li><a href="login.php">Add Places</a></li>
 						</ul>
 				</div><!-- /.navbar-collapse -->
-			</div>   
+			</div>
 		</nav>
 		<!-- Navigation End  -->
-		
+
 		<!-- Main jumbotron for a primary marketing message or call to action -->
 		<section class="main-banner" style="background:#242c36 url(img/background-picture.jpg) no-repeat">
 			<div class="container">
 				<div class="caption">
 					<h2>Visit Amazing places</h2>
-					<form>
-						<fieldset>
-							<div class="col-md-4 col-sm-4 no-pad">
-								<select class="selectpicker border-right">
-								  <option>Country</option>
-								  <option>0 Year</option>
-								  <option>1 Year</option>
-								  <option>2 Year</option>
-								  <option>3 Year</option>
-								</select>
-							</div>
-							<div class="col-md-3 col-sm-3 no-pad">
-								<select class="selectpicker border-right">
-								  <option>Region</option>
-								  <option>0 Year</option>
-								  <option>1 Year</option>
-								  <option>2 Year</option>
-								  <option>3 Year</option>
-								  <option>4 Year</option>
-								</select>
-							</div>
-							<div class="col-md-3 col-sm-3 no-pad">
-								<select class="selectpicker">
-								  <option>Type</option>
-								  <option>Town</option>
-								  <option>City</option>
-								</select>
-							</div>
-							<div class="col-md-2 col-sm-2 no-pad">
-								<input type="submit" class="btn seub-btn" value="Search" />
-							</div>
-						</fieldset>
-					</form>
+					<div class="row">
+                        <div class="col-md-10 col-md-offset-1">
+                            <form>
+                                <fieldset>
+                                    <div class="col-md-10 col-sm-10 no-pad">
+                                        <input type="text" class="form-control"
+                                               placeholder="Search for your Point of Interest">
+                                    </div>
+                                    <div class="col-md-2 col-sm-2 no-pad">
+                                        <input type="submit" class="btn seub-btn" value="Search" />
+                                    </div>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
 				</div>
 			</div>
 		</section>
-		
+
 		<section class="features">
 			<div class="container">
 				<div class="col-md-4 col-sm-4">
@@ -94,7 +76,7 @@
 						</a>
 					</div>
 				</div>
-				
+
 				<div class="col-md-4 col-sm-4">
 					<div class="features-content">
 						<a href="#">
@@ -103,7 +85,7 @@
 						</a>
 					</div>
 				</div>
-				
+
 				<div class="col-md-4 col-sm-4">
 					<div class="features-content">
 						<a href="#">
@@ -112,7 +94,7 @@
 						</a>
 					</div>
 				</div>
-				
+
 				<div class="col-md-4 col-sm-4">
 					<div class="features-content">
 						<a href="#">
@@ -129,7 +111,7 @@
 						</a>
 					</div>
 				</div>
-				
+
 				<div class="col-md-4 col-sm-4">
 					<div class="features-content">
 						<a href="#">
@@ -138,7 +120,7 @@
 						</a>
 					</div>
 				</div>
-			
+
 			</div>
 		</section>
 		<section class="testimonials dark">
@@ -167,7 +149,7 @@
 								<h3 class="testimonial-title">kristiana</h3>
 								<span class="post">Web Designer</span>
 							</div>
-							
+
 							<div class="testimonial">
 								<div class="pic">
 									<img src="img/client-3.jpg" alt="">
@@ -178,7 +160,7 @@
 								<h3 class="testimonial-title">kristiana</h3>
 								<span class="post">Web Designer</span>
 							</div>
-							
+
 							<div class="testimonial">
 								<div class="pic">
 									<img src="img/client-4.jpg" alt="">
@@ -194,12 +176,12 @@
 				</div>
 			</div>
 		</section>
-		
-		
-		
-			
-		
-		 
+
+
+
+
+
+
 		<script type="text/javascript" src="js/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="js/owl.carousel.min.js"></script>
